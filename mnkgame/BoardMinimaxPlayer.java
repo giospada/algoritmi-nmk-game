57c6b47d68679032fffb430deff48296b8c59922
Angelo Huang <xuanqiang.huang@studio.unibo.it>
2022-08-23 18:05:57 +0200

    fix minimax for timeout

--- mnkgame/BoardMinimaxPlayer.java
+++ mnkgame/BoardMinimaxPlayer.java
@@ -4,11 +4,11 @@ public class BoardMinimaxPlayer implements MNKPlayer {
     private CBoard Board;
     private MNKGameState myWin;
     private MNKGameState yourWin;
+    private MNKGameState gameState;
     private int TIMEOUT;
+    private long startTime;
+    private boolean has_timeout;
     private final int kinf = 2;
-    private final int kMyWinValue = 1;
-    private final int kYourWinValue = -1;
-    private final int kDrawValue = 0;
 
     public BoardMinimaxPlayer() {}
 
@@ -17,27 +17,34 @@ public class BoardMinimaxPlayer implements MNKPlayer {
         myWin = first ? MNKGameState.WINP1 : MNKGameState.WINP2;
         yourWin = first ? MNKGameState.WINP2 : MNKGameState.WINP1;
         TIMEOUT = timeout_in_secs;
+        has_timeout = false;
+    }
+
+    public int getValue() {
+        if (gameState == myWin) {
+            return 1;
+        } else if (gameState == yourWin) {
+            return -1;
+        } else {
+            return 0;
+        }
     }
 
     // il giocatore minimo
     public int minPlayer(int alpha, int beta) {
+        has_timeout = (System.currentTimeMillis() - startTime) / 1000.0 > TIMEOUT * (99.0 / 100.0);
+        if (gameState != MNKGameState.OPEN || has_timeout) {
+            return getValue();
+        }
+
         int v = kinf;
         for (CCell cell : Board.getFreeCell()) {
-            MNKGameState newState = Board.markCell(cell.getPosition());
-
-            if (newState != MNKGameState.OPEN)
-                Board.unmarkCell();
-            if (newState == myWin) {
-                continue;
-            } else if (newState == yourWin) {
-                return kYourWinValue;
-            } else if (newState == MNKGameState.DRAW) {
-                v = Math.min(v, kDrawValue);
-                continue;
+            if (has_timeout) {
+                break;
             }
 
+            gameState = Board.markCell(cell.getPosition());
             v = Math.min(v, maxPlayer(alpha, beta));
-
             Board.unmarkCell();
             if (v <= alpha)
                 return v;
@@ -52,26 +59,23 @@ public class BoardMinimaxPlayer implements MNKPlayer {
 
     // il giocatore massimo
     private int maxPlayer(int alpha, int beta) {
-        int v = -kinf;
+        has_timeout = (System.currentTimeMillis() - startTime) / 1000.0 > TIMEOUT * (99.0 / 100.0);
+        if (gameState != MNKGameState.OPEN || has_timeout) {
+            return getValue();
+        }
 
+        int v = -kinf;
         for (CCell cell : Board.getFreeCell()) {
-            MNKGameState newState = MNKGameState.OPEN;
-            newState = Board.markCell(cell.getPosition());
-            if (newState == myWin) {
-                v = Math.max(v, kMyWinValue);
-                Board.unmarkCell();
-                return kMyWinValue;
-            } else if (newState == yourWin) {
-                Board.unmarkCell();
-                continue;
-            } else if (newState == MNKGameState.DRAW) {
-                v = Math.max(v, kDrawValue);
-                Board.unmarkCell();
-                continue;
+            if (has_timeout) {
+                // in teoria se fa break prima di aver fatto una mossa, ritorna un risultato
+                // invalido, il min player vedrebbe la mossa che chiamato questo come la mossa migliore
+                // quindi sceglie per forza questo, ma la probabilità che succeda, dato il check iniziale
+                // di sopra è molto bassa, credo impossibile, solo causato da grande sfortuna.
+                break;  
             }
 
+            gameState = Board.markCell(cell.getPosition());
             v = Math.max(v, minPlayer(alpha, beta));
-
             Board.unmarkCell();
             if (v >= beta)
                 return v;
@@ -82,6 +86,7 @@ public class BoardMinimaxPlayer implements MNKPlayer {
 
     // utilizziamo la board globale per aggiungere e togliere e ci fermiamo quando uno vince
     public MNKCell selectCell(MNKCell[] freeCells, MNKCell[] movedCells) {
+        startTime = System.currentTimeMillis();
         if (movedCells.length > 0) {
             MNKCell c = movedCells[movedCells.length - 1]; // Recover the last move from MC
             Board.markCell(c.i, c.j); // Save the last move in the local MNKBoard
@@ -90,40 +95,27 @@ public class BoardMinimaxPlayer implements MNKPlayer {
         MNKCell bestCell = freeCells[0];
         int v = -kinf;
         int alpha = -kinf;
-        int beta = kinf;
 
         // questo è come se fosse un max player, ma tiene in conto anche della cella
-        for (int i = 0; i < freeCells.length; i++) {
-            MNKGameState newState = MNKGameState.OPEN;
-
-            newState = Board.markCell(freeCells[i].i, freeCells[i].j);
-            if (newState == myWin) {
-                return freeCells[i]; // vintoooo
-            } else if (newState == MNKGameState.DRAW) {
-                if (kDrawValue > v) {
-                    v = kDrawValue;
-                    bestCell = freeCells[i];
-                }
-                Board.unmarkCell();
-                continue;
-            }
-
-            int minPlayerValue = minPlayer(alpha, beta);
+        for (int i = 0; i < freeCells.length && !has_timeout; i++) {
+            gameState = Board.markCell(freeCells[i].i, freeCells[i].j);
+            int minPlayerValue = minPlayer(alpha, kinf);
+            Board.unmarkCell();
             if (minPlayerValue > v) {
                 v = minPlayerValue;
                 bestCell = freeCells[i];
             }
 
-            Board.unmarkCell();
-            if (v >= beta)
-                return bestCell;
             alpha = Math.max(alpha, v);
+            // non faccio check sull'alpha beta al primo livello, perché tanto è impossibile
+            // che sia verificato, beta è sempre kinf
         }
         Board.markCell(bestCell.i, bestCell.j);
+        has_timeout = false;
         return bestCell;
     }
 
     public String playerName() {
-        return "CBoard";
+        return "CBoardMiniMaxPlayer";
     }
 }
