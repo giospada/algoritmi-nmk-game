afe86ee144cb6bbead6d4752cb11d957d0f42238
Angelo Huang <xuanqiang.huang@studio.unibo.it>
2022-09-10 20:35:00 +0200

    maker per la depth

--- utils/maker.py
+++ utils/maker.py
@@ -9,6 +9,7 @@ import os
 import sys
 import argparse
 import subprocess
+import pdb
 
 def main():
     parser = argparse.ArgumentParser(description='aiuta ad eseguire i file in modo più semplice')
@@ -22,11 +23,27 @@ def main():
     parser.add_argument('-p1', '--player1', help='player 1', default=None)
     parser.add_argument('-p2', '--player2', help='player 2', default=None)
     parser.add_argument('-a', '--all', help='simulate prof. test', action='store_true')
+    parser.add_argument('-s', '--bsearch', help='use bin search to find depth', action='store_true')
     args = parser.parse_args()
 
+    if args.bsearch and args.all:
+        for game in all_games:
+            if game[0] <5 or game[1] < 5:
+                continue
+            args.board = game
+            tryValues(args)
+        sys.exit(0)
+
+        
     if args.all:
         play_all(args)
         sys.exit(0)
+        
+    if args.bsearch:
+        tryValues(args)
+        sys.exit(0)
+
+
 
     if args.delete and args.compile:
         raise Exception('You can\'t delete and compile at the same time')
@@ -42,7 +59,81 @@ def main():
     command = make_command(args)
     print(f"executing: {command}")
     os.system(command)
-    
+
+all_games = [
+    (3, 3, 3),
+    (4, 3, 3),
+    (4, 4, 3),
+    (4, 4, 4),
+    (5, 4, 4),
+    (5, 5, 4),
+    (5, 5, 5),
+    (6, 4, 4),
+    (6, 5, 4),
+    (6, 6, 4),
+    (6, 6, 5),
+    (6, 6, 6),
+    (7, 4, 4),
+    (7, 5, 4),
+    (7, 6, 4),
+    (7, 7, 4),
+    (7, 5, 5),
+    (7, 6, 5),
+    (7, 7, 5),
+    (7, 7, 6),
+    (7, 7, 7),
+    (8, 8, 4),
+    (10, 10, 5),
+    (50, 50, 10),
+    (70, 70, 10),
+]
+def writeValueToBigBoard(val,numeroDiMosse):
+    with open('mnkgame/time/DepthValue.java', 'w') as file:
+        file.write("package mnkgame.time;")
+        file.write("public class DepthValue {")
+        file.write("public static final int Depth = " + str(val) + ";")
+        file.write("public static final int NumeroDiMosse= " + str(numeroDiMosse) + ";")
+        file.write("}")
+        file.close()
+
+def tryToExecute(command):
+    out = subprocess.run(command.split(), capture_output=True).stdout.decode().strip().split(os.linesep)
+    res = Output(format_output(out[0]))
+    return res.error == 0
+        
+
+def tryValues(args):
+    with open('./log.txt', 'a') as file:
+        print(f"{args.board=}", file=file)
+        numeroDiMosse = 10000
+        massimo = 3
+        while(numeroDiMosse<10**8):
+            temp = searchTheResult(args,numeroDiMosse,massimo)
+            if temp< massimo :
+                print(f"con {numeroDiMosse} il computer non riesce più a eseguire abbastanza operazioni", file=file)
+                file.flush()
+                break
+            massimo = temp  
+            print(f"{massimo=}, {numeroDiMosse=}", file=file)
+            numeroDiMosse*=10
+            file.flush()
+
+
+
+def searchTheResult(args,numeroMosse,depthVecchia):
+    # pdb.set_trace()
+    hasError = False
+    while not hasError:
+        writeValueToBigBoard(depthVecchia, numeroMosse)
+        compile_all_java()
+        command = make_command(args)
+        if not tryToExecute(command):
+            hasError = True
+        else:
+            depthVecchia+=1
+    return depthVecchia
+
+
 def get_src_dir():
     return os.path.join(os.getcwd(), "mnkgame")
 
@@ -172,33 +263,7 @@ def play_all(args):
     """
     Esegue tutte le partite simulando un test del prof.
     """
-    all_games = [
-        (3, 3, 3),
-        (4, 3, 3),
-        (4, 4, 3),
-        (4, 4, 4),
-        (5, 4, 4),
-        (5, 5, 4),
-        (5, 5, 5),
-        (6, 4, 4),
-        (6, 5, 4),
-        (6, 6, 4),
-        (6, 6, 5),
-        (6, 6, 6),
-        (7, 4, 4),
-        (7, 5, 4),
-        (7, 6, 4),
-        (7, 7, 4),
-        (7, 5, 5),
-        (7, 6, 5),
-        (7, 7, 5),
-        (7, 7, 6),
-        (7, 7, 7),
-        (8, 8, 4),
-        (10, 10, 5),
-        (50, 50, 10),
-        (70, 70, 10),
-    ]
+
 
     # overwrite existings args, so that is default
     args.rounds = 1
